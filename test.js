82641dec73bef527a60b019dd495c6e86e42198d
Virej Dasani <dasanivirej@gmail.com>
2021-01-27 11:09:31 +0530

    Changed function name

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 let nameButton = document.getElementById("addButton")
 displayMeetings()
+displayMeetingLink()
 
-// On add button click
+// On add button click for name
 nameButton.addEventListener("click", function (e) {
     let nameInput = document.getElementById("nameInput")
 
@@ -30,9 +31,40 @@ nameButton.addEventListener("click", function (e) {
     console.log(localStorage)
 
     displayMeetings()
+    displayMeetingLink()
 })
 
-// To show notes in cards in the app
+// On add button click for link
+nameButton.addEventListener("click", function (e) {
+    let linkInput = document.getElementById("linkInput")
+
+    // Fetch the meeting link from local 
+    let meetingLink = localStorage.getItem("meetingLink")
+
+    // In case it doesn't exist
+    if (meetingLink === null) {
+        meetingLinkArray = []
+    } else {
+        // Else, assign the input to meetingLinkArray
+        meetingLinkArray = JSON.parse(meetingLink)
+    }
+
+    // Push the input link value to meetingLinkArray
+    meetingLinkArray.push(linkInput.value)
+    console.log(linkInput.value)
+
+    // Set it in local storage in string format
+    localStorage.setItem("meetingLink", JSON.stringify(meetingLinkArray))
+
+    // Clear the input field
+    linkInput.value = ""
+
+    console.log(meetingLinkArray + " = meetingLinkArray")
+    console.log(localStorage)
+})
+
+
+// To show meeting names in the cards
 function displayMeetings() {
     // Fetch the meeting name from local 
     let meetingName = localStorage.getItem("meetingName")
@@ -54,7 +86,7 @@ function displayMeetings() {
             <div class="card" style="width: 18rem;">
                 <div class="card-body">
                     <h5 class="card-title">${element}</h5>
-                    <p class="card-text">link</p>
+                    <p id="meetingLink" class="card-text">text</p>
                     <button id="${index}" onclick="deleteMeeting(this.id)" class="btn btn-primary">Delete</button>
                 </div>
             </div>
@@ -63,15 +95,45 @@ function displayMeetings() {
     })
     let meetingNameElem = document.getElementById("meetings")
     // If meetings exist
-    if (meetingNameArray .length != 0) {
+    if (meetingNameArray.length != 0) {
         meetingNameElem.innerHTML = cardHTML
     }
     // If there are no meetings
     else {
         meetingNameElem.innerHTML = `<div id="centre">You have no meetings. Add a meeting from above!</div>`
     }
-    
+
 }
+
+// To show link in the cards
+function displayMeetingLink() {
+    let meetingLink = localStorage.getItem("meetingLink")
+    // In case it doesn't exist
+    if (meetingLink === null) {
+        meetingLinkArray = []
+    } else {
+        // Else, assign the input to meetingLinkArray
+        meetingLinkArray = JSON.parse(meetingLink)
+    }
+
+    // Empty at first
+    let linkHTML = ""
+
+
+    meetingLinkArray.forEach(function (element, index) {
+        linkHTML = `${element}`
+    })
+    let meetingLinkElem = document.getElementById("meetingLink")
+    // If meetings exist
+    if (meetingLinkArray.length != 0) {
+        meetingLinkElem.innerHTML = linkHTML
+    }
+    // If there are no meetings
+    else {
+        meetingLinkElem.innerHTML = `<div id="centre">You have not entered a link for this meeting.</div>`
+    }
+}
+
 
 // Deleting a meeting
 function deleteMeeting(index) {
@@ -94,6 +156,6 @@ function deleteMeeting(index) {
     localStorage.setItem("meetingName", JSON.stringify(meetingNameArray))
     // Refresh and display the notes again
     displayMeetings()
-
-
+    // displayMeetingLink()
 }
+displayMeetingLink()
